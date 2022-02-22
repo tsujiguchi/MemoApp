4c812fe5e013317911dbca458f442e8f6e17afc7
tsujiguchi <tsujiguchi@careerindex.co.jp>
2022-02-22 16:09:58 +0900

    Replace KeyboardSafeView

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
-    <MemoDetailScreen />
-    // <MemoEditScreen />
+    // <MemoDetailScreen />
+    <MemoEditScreen />
     // <MemoCreateScreen />
   );
 }
--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,18 +1,19 @@
 import {
-  View, StyleSheet, TextInput, KeyboardAvoidingView,
+  View, StyleSheet, TextInput,
 } from 'react-native';
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
+import KeyboardSafeView from '../components/KeyBoardSafeView';
 
 export default function MemoCreateScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
 
